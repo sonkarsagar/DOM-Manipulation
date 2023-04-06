5b827f66a3ca6b8404288bde0af09733ef6a265e
sonkarsagar <sagarsonker56625@gmail.com>
2023-04-06 22:53:27 +0530

    first commit

--- dom.js
+++ dom.js
@@ -13,9 +13,24 @@ divtitle[1].innerHTML="<h4>Vastuyein</h4>"
 
 form[0].value="Jama Karein"
 itemslists1[0].textContent="Vastu 1"
+itemslists1[0].style.color="Red"
+itemslists1[0].style.fontWeight="Bold"
+
+// itemslists1[0].style="Red"
+
 itemslists1[1].textContent="Vastu 2"
+itemslists1[1].style.color="Blue"
+itemslists1[1].style.fontWeight="Bold"
+
 itemslists1[2].textContent="Vastu 3"
+itemslists1[2].style.background="green"
+itemslists1[2].style.color="Black"
+itemslists1[2].style.fontWeight="Bold"
+
 itemslists1[3].textContent="Vastu 4"
+itemslists1[3].style.color="Green"
+itemslists1[3].style.fontWeight="Bold"
+
 header.style.borderBottom="solid 3px #000"
 header.style.borderTop="solid 3px #000"
 header.style.borderLeft="solid 3px #000"
